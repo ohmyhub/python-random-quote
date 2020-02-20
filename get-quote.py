f249466cfc8ca4167c4c959b71e64ef24ee872c0
Kevin <k.warden.89@gmail.com>
2020-02-20 12:52:34 -0800

    I removed the newline from the print statements

--- get-quote.py
+++ get-quote.py
@@ -8,7 +8,7 @@ def primary():
       f.close()
       last = 15
       rnd = random.randint(0, last)
-      print(quotes[rnd])
+      print(quotes[rnd], end='')
 
 if __name__== "__main__":
   primary()
