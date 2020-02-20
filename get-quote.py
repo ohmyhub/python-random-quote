124527412f1890cbb25fcdfc8a9a9dc4adca04ad
Kevin <k.warden.89@gmail.com>
2020-02-20 12:10:28 -0800

    It spits out multiple quotes now

--- get-quote.py
+++ get-quote.py
@@ -2,13 +2,13 @@ import random
 
 def primary():
    # print("Keep it logically awesome.")
-
-   f = open("quotes.txt")
-   quotes = f.readlines()
-   f.close()
-   last = 15
-   rnd = random.randint(0, last)
-   print(quotes[rnd])
+   for i in range(5):
+      f = open("quotes.txt")
+      quotes = f.readlines()
+      f.close()
+      last = 15
+      rnd = random.randint(0, last)
+      print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
